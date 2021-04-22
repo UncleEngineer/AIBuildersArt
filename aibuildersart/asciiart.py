1455175281b5f4a04deaaaab988eeeac8481d71f
Meen Anusak <meenanusak@gmail.com>
2021-04-22 22:07:36 +0700

    feat: Add MeenAnusak Class

--- aibuildersart/asciiart.py
+++ aibuildersart/asciiart.py
@@ -3,6 +3,7 @@ class UncleEngineer:
     test = UncleEngineer()
     test.art()
     '''
+
     def __init__(self):
         self.name = 'Uncle Engineer'
 
@@ -34,6 +35,41 @@ class UncleEngineer:
       `-| '
         '''
         print(asciiart)
-        
+
+    def __str__(self):
+        return self.name
+
+
+class MeenAnusak:
+    '''
+    test = MeenAnusak()
+    test.art()
+    '''
+
+    def __init__(self):
+        self.name = 'Meen Anusak'
+
+    def art(self):
+        asciiart = '''
+             _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     `-'       `--' hjm
+
+        '''
+        print(asciiart)
+
     def __str__(self):
         return self.name
