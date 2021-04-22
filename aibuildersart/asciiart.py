228324eea37cff772883b59df5c4c4d190c9c943
Apichai <thinthonglang.apichai@gmail.com>
2021-04-22 22:27:46 +0700

    Apichai Thinthonglang

--- aibuildersart/asciiart.py
+++ aibuildersart/asciiart.py
@@ -37,3 +37,39 @@ class UncleEngineer:
         
     def __str__(self):
         return self.name
+
+class ApichaiCoding:
+    '''
+    test = UncleEngineer()
+    test.art()
+    '''
+    def __init__(self):
+        self.name = 'Uncle Engineer'
+
+    def art(self):
+        asciiart = '''
+~~~~~~~~~~~~~~~~~~~~~~~~~        
+            /|      __
+           / |   ,-~ /
+          Y :|  //  /
+          | jj /( .^
+          >-"~"-v"
+         /       Y
+        jo  o    |
+       ( ~T~     j
+        >._-' _./
+       /   "~"  |
+      Y     _,  |
+     /| ;-"~ _  l
+    / l/ ,-"~    \
+    \//\/      .- \
+     Y        /    Y    -saveralph
+     l       I     !
+     ]\      _\    /"\
+    (" ~----( ~   Y.  )
+~~~~~~~~~~~~~~~~~~~~~~~~~
+        '''
+        print(asciiart)
+        
+    def __str__(self):
+        return self.name
