41730d0075884293dd53b104b724b3043ac1881d
Wisarut Si <wisarut.sirimart@gmail.com>
2021-04-22 22:32:51 +0700

    add wisarut

--- aibuildersart/asciiart.py
+++ aibuildersart/asciiart.py
@@ -37,3 +37,21 @@ class UncleEngineer:
         
     def __str__(self):
         return self.name
+
+class WisarutSiCMU:
+    def __init__(self):
+        self.name = 'Wisarut Sirimart'
+
+    def art(self):
+        asciiart = '''
+                     ((`\\
+            ___ \\ '--._
+         .'`   `'  O  o )   wisarut
+        /    \   '. __.'
+      __|    /_  \ \_\_
+     {__\______\-'\__\_\\
+        '''
+        print(asciiart)
+        
+    def __str__(self):
+        return self.name
