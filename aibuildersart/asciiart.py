b20091eb2b969b0ca58fde27e8c41c0603e5ac39
= <loong.wissawakorn@gmail.com>
2021-04-22 22:24:46 +0700

    add dekchai tortao

--- aibuildersart/asciiart.py
+++ aibuildersart/asciiart.py
@@ -37,3 +37,44 @@ class UncleEngineer:
         
     def __str__(self):
         return self.name
+
+
+
+class DekchaiTortao:
+    '''
+    test = DekchaiTortao()
+    test.art()
+    '''
+    def __init__(self):
+        self.name = 'Dekchai Tortao Jaaaa'
+
+    def art(self):
+        asciiart = '''
+---------------------------
+       .---.
+  ___ /_____\\
+ /\.-`( '.' )
+/ /    \_-_/_    Dekchai
+\ `-.-"`'V'//-.    Tortao
+ `.__,   |// , \     Krubpom
+     |Ll //Ll|\ \\
+     |__//   | \_\\
+    /---|[]==| / /
+    \__/ |   \/\/
+    /_   | Ll_\|
+     |`^"""^`|
+     |   |   |
+     |   |   |
+     |   |   |
+     |   |   |
+     L___l___J
+ jgs  |_ | _|
+     (___|___)
+      ^^^ ^^^
+--------------------------- 
+        '''
+        print(asciiart)
+        
+    def __str__(self):
+        return self.name
+
