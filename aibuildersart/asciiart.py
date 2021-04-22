aed9c1a3161695a40fae701236becf0c0222ce1b
Oran Kobkaew <22838b@gmail.com>
2021-04-22 22:26:13 +0700

    add munkkkkkk

--- aibuildersart/asciiart.py
+++ aibuildersart/asciiart.py
@@ -37,3 +37,41 @@ class UncleEngineer:
         
     def __str__(self):
         return self.name
+
+class munkkkkkk:
+    '''
+    test = munkkkkkk()
+    test.art()
+    '''
+    def __init__(self):
+        self.name = 'munkkkkkk'
+
+    def art(self):
+        asciiart = '''
+        .-""""--.
+      /         )
+     /      --"`
+    /       _`:---.
+   |     .-'       `\
+    \   /    .----'./
+     \  : ,-' ~(.).)\
+      \_| \      ._) |
+       /   |  \.__, /
+  _.--'    )`///-,-'
+ /        / _| (_\\
+|        (____/____)
+ \     ___/       | _
+  `---(            ` )
+       `-,          .'
+        (__.'._/'._/
+             |`| |
+          __/ / /
+         //   | `--.
+        ||    /_____)
+  jgs   `=---`
+        '''
+        print(asciiart)
+        
+    def __str__(self):
+        return self.name
+
