b7da3e46d4bd339ba9f5946d235a018505648789
lanuxos <lanuxos@gmail.com>
2021-05-24 19:24:22 +0700

    add Lasciiart class

--- aibuildersart/asciiart.py
+++ aibuildersart/asciiart.py
@@ -151,3 +151,37 @@ class Batman:
         
     def __str__(self):
         return self.name
+
+
+class Lasciiart:
+    '''
+    test = UncleEngineer()
+    test.art()
+    '''
+
+    def __init__(self):
+        self.name = 'Uncle Engineer'
+
+    def art(self):
+        asciiart = '''
+_nnnn_                      
+        dGGGGMMb     ,"""""""""""""".
+       @p~qp~~qMb    | Lascii Art   |
+       M|@||@) M|   _;..............'
+       @,----.JM| -'
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMM|   .'
+     `-'       `--' hjm
+        '''
+        print(asciiart)
+
+    def __str__(self):
+        return self.name
