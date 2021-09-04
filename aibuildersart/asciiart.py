e00fe0bd463326c2114dd1c52c392561d54ae36e
sira <sira.programmer@gmail.com>
2021-09-04 21:03:26 +0700

    add sira git

--- aibuildersart/asciiart.py
+++ aibuildersart/asciiart.py
@@ -38,138 +38,23 @@ class UncleEngineer:
     def __str__(self):
         return self.name
 
-class Ridwan0019:
+
+
+class SiraArt:
     '''
-    test = Ridwan0019()
+    test = SiraArt()
     test.art()
     '''
     def __init__(self):
-        self.name = 'Ridwan0019'
+        self.name = 'SiraArt'
 
     def art(self):
         asciiart = '''
- ------------------------------------------------       
-        (\ 
-        \'\ 
-        \'\     __________  
-        / '|   ()_________)
-        \ '/    \ ~~~~~~~~ \
-         \       \ ~~~~~~   \
-        ==).      \__________\
-       (__)       ()__________)  Ridwan0019
-------------------------------------------------
+ /\_/\ 
+( o.o )
+ > ^ <                            
         '''
         print(asciiart)
         
     def __str__(self):
         return self.name
-
-
-
-class GuitarDev:
-    '''
-    test = UncleEngineer()
-    test.art()
-    '''
-    def __init__(self):
-        self.name = 'GuitarDev'
-
-    def art(self):
-        asciiart = '''
-                                                                   . ,,                 
-                                                         ,(&&/   %/             
-                                                       /##%&.    &@%            
-                                                       ,%%     *@&%.            
-                                                        &,    ,@&(              
-                                                       *&.#&&#% ,.              
-                                                      %* .&,                    
-                                                    *&  .%                      
-                                                   &(. #*                       
-                                                 *&/ .&.                        
-                                               .&#/ *#                          
-                                              /##/.%*                           
-                                             &,%*.&.                            
-                                           /(.#*/#                              
-                               ,#@&&&@#, .@..%*%.                               
-                             &,         (( .%/%                                 
-                            %.        .&. ,#%/       I'm Guitar Dev.                      
-                           ,#        #(  .%&.                                   
-                          .&    *&#//#&(,%%&,                                   
-                       ./&,   #(&*    ,&*%   /&%/,                              
-                 ,%&,        ,(%        %/(      ,%                             
-              *&.             &#*      ,&(,    *&.                              
-            *%                 ##(&%%%#(%.  .&*                                 
-           (/                              .%                                   
-          .%         .&/                   .%                                   
-          ,#         (* .%#.                %.                                  
-           &.          .&   ,%%*            (,                                  
-           ,#            ,%(,.*, .##        %.                                  
-            .&.                 ..         ,#                                   
-              ,&.                         ,%                                    
-                 ##.                    .%*                                     
-                    .%&,              (&.                                       
-                          .*(#%&%#(,                                            
-        '''
-        print(asciiart)
-        
-    def __str__(self):
-        return self.name
-
-class Batman:
-    '''
-    test = Batman()
-    test.art()
-    '''
-    def __init__(self):
-        self.name = 'Jman'
-
-    def art(self):
-        asciiart = '''
-                   ,.ood888888888888boo.,
-              .od888P^""            ""^Y888bo.
-          .od8P''   ..oood88888888booo.    ``Y8bo.
-       .odP'"  .ood8888888888888888888888boo.  "`Ybo.
-     .d8'   od8'd888888888f`8888't888888888b`8bo   `Yb.
-    d8'  od8^   8888888888[  `'  ]8888888888   ^8bo  `8b
-  .8P  d88'     8888888888P      Y8888888888     `88b  Y8.
- d8' .d8'       `Y88888888'      `88888888P'       `8b. `8b
-.8P .88P            """"            """"            Y88. Y8.
-88  888                                              888  88
-88  888                                              888  88
-88  888.        ..                        ..        .888  88
-`8b `88b,     d8888b.od8bo.      .od8bo.d8888b     ,d88' d8'
- Y8. `Y88.    8888888888888b    d8888888888888    .88P' .8P
-  `8b  Y88b.  `88888888888888  88888888888888'  .d88P  d8'
-    Y8.  ^Y88bod8888888888888..8888888888888bod88P^  .8P
-     `Y8.   ^Y888888888888888LS888888888888888P^   .8P'
-       `^Yb.,  `^^Y8888888888888888888888P^^'  ,.dP^'
-          `^Y8b..   ``^^^Y88888888P^^^'    ..d8P^'
-              `^Y888bo.,            ,.od888P^'
-                   "`^^Y888888888888P^^'"
-        '''
-        print(asciiart)
-        
-    def __str__(self):
-        return self.name
-
-class kongNeko:
-    '''
-    test = kongNeko()
-    test.art()
-    '''
-    def __init__(self):
-        self.name = 'kongNeko Krup'
-
-    def art(self):
-        asciiart = '''
- |\__/,|   (`\
- |_ _  |.--.) )   kongNeko
- ( T   )     /           Cat
-(((^_(((/(((_/
-        '''
-        print(asciiart)
-        
-    def __str__(self):
-        return self.name
-
-
